427484c86b84a875e04ac33e2423573426637e36
Ahmed-Samy-2003 <aahmedsamy222@gmail.com>
2025-03-05 14:54:29 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
 quiz.js doesn't contain: ["checkAnswer"]
 quiz.js doesn't contain: ["correctAnswer", "4"]
 quiz.js doesn't contain: ["document.querySelector", "checked", "value"] / quiz.js doesn't contain: ["userAnswer"]
+quiz.js doesn't contain: ["if (userAnswer === correctAnswer)"]
